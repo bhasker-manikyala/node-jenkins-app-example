069662b07e6c7c769a979397e34b4c6262cffa87
bhasker-manikyala <28338138+bhasker-manikyala@users.noreply.github.com>
2025-10-12 22:53:52 +0530

    Update helloWorld-spec.js

--- spec/helloWorld-spec.js
+++ spec/helloWorld-spec.js
@@ -13,7 +13,7 @@ describe("Hello World Server", function() {
 
     it("returns Hello World", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Welcome to Docker!");
+        expect(body).toBe("Welcome to Docker Live Training!");
         done();
 
         server.close();
