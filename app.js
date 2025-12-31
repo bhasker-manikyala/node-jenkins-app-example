a474257b199cfa39ae52f2e22fa929f2e42ca4eb
bhasker-manikyala <28338138+bhasker-manikyala@users.noreply.github.com>
2025-12-31 12:36:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Welcome to Docker Live Training!'))
+app.get('/', (req, res) => res.send('Welcome to Edureka Online Training!'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
